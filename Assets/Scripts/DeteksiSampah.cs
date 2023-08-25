81ea8a235370498084b90c7491171870d16a844e
yolonly03 <ngawurmeneh12@gmail.com>
2023-08-25 15:48:55 +0700

    MemilahSampah

---
+++ Assets/Scripts/DeteksiSampah.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeteksiSampah : MonoBehaviour
+{
+    public string nameTag;
+    public AudioClip audioBenar;
+    public AudioClip audioSalah;
+    private AudioSource MediaPlayerBenar;
+    private AudioSource MediaPlayerSalah;
+    public Text textScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MediaPlayerBenar = gameObject.AddComponent<AudioSource>();
+        MediaPlayerBenar.clip = audioBenar;
+
+        MediaPlayerSalah = gameObject.AddComponent<AudioSource>();
+        MediaPlayerSalah.clip = audioSalah;
+    }
+    void OnTriggerEnter2D(Collider2D collision){
+        if (collision.tag.Equals(nameTag)){
+            Data.score += 25;
+            textScore.text = Data.score.ToString();
+            Destroy(collision.gameObject);
+            MediaPlayerBenar.Play();
+        }
+        else{
+            Data.score -= 5;
+            textScore.text = Data.score.ToString();
+            Destroy(collision.gameObject);
+            MediaPlayerSalah.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    void Start() {
+        
+    }
+    float timer = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 2){
+            Data.score <<= 0;
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+}
